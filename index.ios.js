c23b15e7b61811de66a2c77e9865010717e3cece
alexander ames <alexanderthomasames@gmail.com>
2017-01-29 20:01:52 -0700

    init repo

---
+++ index.ios.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var React = require('react');
+var ReactNative = require('react-native');
+
+var styles = ReactNative.StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  },
+  container: {
+    flex: 1
+  }
+});
+
+class HelloWorld extends React.Component {
+  render() {
+    return <ReactNative.Text style={styles.text}>Hello World (Again)</ReactNative.Text>;
+  }
+}
+class anwbgning extends React.Component {
+  render() {
+    return (
+      <ReactNative.NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'A New Begining',
+          component: HelloWorld,
+        }}/>
+    );
+  }
+}
+
+
+ReactNative.AppRegistry.registerComponent('anwbgning', function() { return anwbgning });
