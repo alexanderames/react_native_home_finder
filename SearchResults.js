b8eecab5c4bceaf1871872f596e4ef79bf7f11ec
alexander ames <alexanderthomasames@gmail.com>
2017-02-01 10:30:12 -0700

    create Property Details view

--- SearchResults.js
+++ SearchResults.js
@@ -10,6 +10,38 @@ import {
   Text
 } from 'react-native';
 
+
+var PropertyView = require('./PropertyView');
+
+var styles = StyleSheet.create({
+  thumb: {
+    width: 80,
+    height: 80,
+    marginRight: 10
+  },
+  textContainer: {
+    flex: 1
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#dddddd'
+  },
+  price: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#48BBEC'
+  },
+  title: {
+    fontSize: 20,
+    color: '#656565'
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    padding: 10
+  }
+});
+
+
 class SearchResults extends Component {
  
   constructor(props) {
@@ -20,17 +52,37 @@ class SearchResults extends Component {
       dataSource: dataSource.cloneWithRows(this.props.listings)
     };
   }
+
+	rowPressed(listerURL) {
+	  var property = this.props.listings.filter(prop => prop.lister_url === listerURL)[0];
+	 
+	  this.props.navigator.push({
+	    title: "Property",
+	    component: PropertyView,
+	    passProps: {property: property}
+	  });
+	}
  
-  renderRow(rowData, sectionID, rowID) {
-    return (
-      <TouchableHighlight
-          underlayColor='#dddddd'>
-        <View>
-          <Text>{rowData.title}</Text>
-        </View>
-      </TouchableHighlight>
-    );
-  }
+	renderRow(rowData, sectionID, rowID) {
+	  var price = rowData.price_formatted.split(' ')[0];
+	 
+	  return (
+	    <TouchableHighlight onPress={() => this.rowPressed(rowData.lister_url)}
+	        underlayColor='#dddddd'>
+	      <View>
+	        <View style={styles.rowContainer}>
+	          <Image style={styles.thumb} source={{ uri: rowData.img_url }} />
+	          <View  style={styles.textContainer}>
+	            <Text style={styles.price}>{price}</Text>
+	            <Text style={styles.title}
+	                  numberOfLines={1}>{rowData.title}</Text>
+	          </View>
+	        </View>
+	        <View style={styles.separator}/>
+	      </View>
+	    </TouchableHighlight>
+	  );
+	}
  
   render() {
     return (
